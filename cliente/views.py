36ac58ae40a174b9be768b6a79018208d5451ea5
Pedro <pedro.pacheco.a@gmail.com>
2016-03-23 10:17:27 -0300

    Individual

--- cliente/views.py
+++ cliente/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 
 def cliente_list(request):
     posts = Post.objects.filter()
     return render(request, 'cliente/cliente_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'cliente/cliente_detail.html', {'post': post})
